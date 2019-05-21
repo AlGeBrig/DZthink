1a557269d08ea73867fdbca7d9e2124c14d9ef31
AlGeBrig <avenon7@gmail.com>
2019-05-21 11:22:36 +0300

    4 задача решена

---
+++ equation.rb
@@ -0,0 +1,21 @@
+puts "Введите коэффициент a"
+a = gets.chomp.to_i
+
+puts "Введите коэффициент b"
+b = gets.chomp.to_i
+
+puts "Введите коэффициент c"
+c = gets.chomp.to_i
+
+d = b * b - 4 * a * c 
+
+if d > 0
+    x1 = (- b + Math.sqrt(d)) / (2 * a)
+    x2 = (- b - Math.sqrt(d)) / (2 * a)
+    puts "D = #{d}, X1 = #{x1}, X2 = #{x2}"
+elsif d == 0
+    x = -b / 2 * a 
+    puts "D = #{d}, X = #{x}"
+elsif d < 0 
+    puts "Корней нет"
+end
