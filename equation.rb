08dfe4b909b8b5db922ba60fcd8886285a732710
AlGeBrig <avenon7@gmail.com>
2019-05-22 14:19:57 +0300

    Поправил отступы

--- equation.rb
+++ equation.rb
@@ -10,12 +10,12 @@ c = gets.chomp.to_i
 d = b * b - 4 * a * c 
 
 if d > 0
-    x1 = (- b + Math.sqrt(d)) / (2 * a)
-    x2 = (- b - Math.sqrt(d)) / (2 * a)
-    puts "D = #{d}, X1 = #{x1}, X2 = #{x2}"
+  x1 = (- b + Math.sqrt(d)) / (2 * a)
+  x2 = (- b - Math.sqrt(d)) / (2 * a)
+  puts "D = #{d}, X1 = #{x1}, X2 = #{x2}"
 elsif d == 0
-    x = -b / 2 * a 
-    puts "D = #{d}, X = #{x}"
+  x = -b / 2 * a 
+  puts "D = #{d}, X = #{x}"
 elsif d < 0 
-    puts "Корней нет"
+  puts "Корней нет"
 end
